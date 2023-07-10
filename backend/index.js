edc19518e354f9f9623085081f4c39d94b9af55c
Gaurav Goswami <gauravgoswami.aug@gmail.com>
2023-07-11 02:28:57 +0530

    added necessary routes and configuration in index file (backend)

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,13 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
+const userRoutes = require("./routes/User");
+const profileRoutes = require("./routes/Profile");
+const paymentRoutes = require("./routes/Payment");
+const courseRoutes = require("./routes/Course");
+const cloudinaryConnect = require("./config/cloudinary");
+const fileUpload = require("express-fileupload");
+
 dotenv.config({
     path : './config/config.env'
 })
@@ -23,6 +30,22 @@ app.use(cors({
     methods : ["GET" , "POST" , "PUT" , "DELETE"],
     credentials : true
 }))
+
+app.use(
+	fileUpload({
+		useTempFiles:true,
+		tempFileDir:"/tmp",
+	})
+)
+//cloudinary connection
+cloudinaryConnect();
+
+//routes
+app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/profile", profileRoutes);
+app.use("/api/v1/course", courseRoutes);
+app.use("/api/v1/payment", paymentRoutes);
+
 
 app.listen(PORT , () => {
     console.log(`Server is running at PORT ${PORT}`)
