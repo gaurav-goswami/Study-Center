3c932f65194934261de6ea3c31a71497daacc9fa
Gaurav Goswami <gauravgoswami.aug@gmail.com>
2023-07-13 17:44:03 +0530

    added our-mission section animation

--- client/src/components/About/OurMission.jsx
+++ client/src/components/About/OurMission.jsx
@@ -3,50 +3,69 @@ import Heading from "../common/Heading";
 import Paragraph from "../common/Paragraph";
 import {FaReact, FaNodeJs, FaDocker, FaAws, FaHtml5, FaCss3Alt, FaPython} from "react-icons/fa";
 import {SiMongodb, SiTypescript, SiNginx} from "react-icons/si";
+import "./OurMission.css";
 
 const OurMission = () => {
 
     const iconBall = [
         {
-            icon : <FaReact className="text-3xl react-icon"/>
+            icon : <FaReact className="text-4xl"/>,
+            name : "react"
         },
         {
-            icon : <FaNodeJs className="text-3xl node-icon"/>
+            icon : <FaNodeJs className="text-4xl"/>,
+            name : "node"
         },
         {
-            icon : <FaDocker className="text-3xl docker-icon"/>
+            icon : <FaHtml5 className="text-4xl"/>,
+            name : "html"
         },
         {
-            icon : <FaAws className="text-3xl aws-icon"/>
+            icon : <FaDocker className="text-4xl"/>,
+            name : "docker"
         },
         {
-            icon : <FaHtml5 className="text-3xl html-icon"/>
+            icon : <FaAws className="text-4xl"/>,
+            name : "aws"
         },
         {
-            icon : <FaCss3Alt className="text-3xl css-icon"/>
+            icon : <FaCss3Alt className="text-4xl"/>,
+            name : "css"
         },
         {
-            icon : <FaPython className="text-3xl python-icon"/>
+            icon : <FaPython className="text-4xl"/>,
+            name : "python"
         },
         {
-            icon : <SiMongodb className="text-3xl mongo-icon"/>
+            icon : <SiMongodb className="text-4xl"/>,
+            name : "mongo"
         },
         {
-            icon : <SiTypescript className="text-3xl ts-icon"/>
+            icon : <SiTypescript className="text-4xl"/>,
+            name : "ts"
         },
         {
-            icon : <SiNginx className="text-3xl nginx-icon"/>
+            icon : <SiNginx className="text-4xl"/>,
+            name : "nginx"
         }
     ]
 
     return (
     <>
-      <div className="w-[90%] md:w-[80%] xl:w-[75%] mx-auto my-4 flex flex-col md:flex-row gap-2 bg-caribbeangreen-300 p-2">
+      <div className="w-[90%] md:w-[80%] xl:w-[75%] mx-auto my-4 flex flex-col md:flex-row gap-2 p-2">
         {/* animation */}
 
-        <div className="w-[100%] md:w-[40%] p-2 bg-yellow-300"></div>
+        <div className="w-[100%] md:w-[40%] p-2 flex flex-wrap">
+            {
+                iconBall.map((item, index) => {
+                    return <div className={`p-2 bg-pure-greys-5 w-fit m-2 rounded-full overflow-hidden icon-container relative h-fit icon-${item.name}`} key={index}>
+                        {item.icon}
+                    </div>
+                })
+            }        
+        </div>
 
-        <div className="w-[100%] md:w-[60%] p-2 bg-yellow-100">
+        <div className="w-[100%] md:w-[60%] p-2">
           <Heading style="text-xl md:text-5xl font-extrabold text-yellow-200">Our Mission</Heading>
           <Paragraph color="text-pure-greys-100" styles="text-lg md:text-xl my-2">At Study Center, our mission is to empower learners with the knowledge and skills needed to excel in their educational and professional journeys. We are dedicated to providing high-quality, accessible, and engaging learning resources that foster a deep understanding of various subjects.
           Our goal is to create a supportive and inclusive learning environment where individuals can explore their passions, expand their horizons, and unlock their full potential. We believe in the transformative power of education and its ability to shape lives positively.</Paragraph>
@@ -54,7 +73,7 @@ const OurMission = () => {
           <Paragraph color="text-pure-greys-100" styles="text-lg md:text-xl my-2">Through our team of experienced instructors, industry experts, and top professionals, we strive to deliver courses that are practical, relevant, and aligned with real-world needs. We aim to bridge the gap between theory and practice by offering hands-on projects, interactive exercises, and practical assignments.
           At Study Center, we embrace continuous learning, innovation, and staying ahead of the curve in an ever-evolving world. We are committed to nurturing a community of lifelong learners who are driven by curiosity and a thirst for knowledge.</Paragraph>
 
-          <Heading style="text-xl md:text-2xl text-pink-300 my-5">Welcome to Study Center – Where Learning Knows No Boundaries!</Heading>
+          <Heading style="text-xl md:text-2xl text-pink-400 my-5">Welcome to Study Center – Where Learning Knows No Boundaries!</Heading>
         </div>
       </div>
     </>
--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -12,7 +12,7 @@ const AboutPage = () => {
         <MainWrapper>
             <div className='min-h-screen'>
 
-                <div className='w-[90%] md:w-[80%] xl:w-[70%] mx-auto flex gap-2 my-5 flex-col md:flex-row bg-yellow-300'>
+                <div className='w-[90%] md:w-[80%] xl:w-[70%] mx-auto flex gap-2 my-5 flex-col md:flex-row'>
                   <div className='md:w-[50%] w-[100%]'>
                     <Heading style="text-3xl md:text-5xl font-extrabold text-yellow-200">Our Story</Heading>
 
@@ -21,7 +21,7 @@ const AboutPage = () => {
                     As experienced educators ourselves, we witnessed firsthand the limitations and challenges of traditional education systems. We believed that education should not be confined to the walls of a classroom or restricted by geographical boundaries. We envisioned a platform that could bridge these gaps and empower individuals from all walks of life to unlock their full potential.</Paragraph>
                   </div>
 
-                  <div className='p-5 md:w-[50%] w-[100%] bg-caribbeangreen-600'>
+                  <div className='p-5 md:w-[50%] w-[100%]'>
                     <img src={aboutImage} alt="image" className='w-[100%] h-[100%] object-cover object-center'/>
                   </div>
 
