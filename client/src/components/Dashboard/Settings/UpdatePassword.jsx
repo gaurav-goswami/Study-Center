f09f1aa0ad2f9adec05949a415a520ca5735a5c4
Gaurav Goswami <gauravgoswami.aug@gmail.com>
2023-07-20 23:45:49 +0530

    added change password ui on dashboard

--- client/src/components/Dashboard/Settings/UpdatePassword.jsx
+++ client/src/components/Dashboard/Settings/UpdatePassword.jsx
@@ -1,11 +1,102 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 
 const UpdatePassword = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [showOldPassword, setShowOldPassword] = useState(false);
+  const [showNewPassword, setShowNewPassword] = useState(false);
 
-export default UpdatePassword
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  return (
+    <>
+      <form>
+        <div className="my-10 flex flex-col gap-y-6 rounded-md border-[1px] border-richbl-700 bg-richblue-800 p-8 px-12">
+          <h2 className="text-lg font-semibold text-richblack-5">Password</h2>
+          <div className="flex flex-col gap-5 lg:flex-row">
+            <div className="relative flex flex-col gap-2 lg:w-[48%]">
+              <label htmlFor="oldPassword" className="text-pure-greys-200">
+                Current Password
+              </label>
+              <input
+                type={showOldPassword ? "text" : "password"}
+                name="oldPassword"
+                id="oldPassword"
+                placeholder="Enter Current Password"
+                className="outline-none px-2 py-4 rounded-md text-pure-greys-25 bg-richblue-700 placeholder:text-pure-greys-400 w-[100%]"
+                {...register("oldPassword", { required: true })}
+              />
+              <span
+                onClick={() => setShowOldPassword((prev) => !prev)}
+                className="absolute right-3 top-[50px] z-[10] cursor-pointer"
+              >
+                {showOldPassword ? (
+                  <AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF" />
+                ) : (
+                  <AiOutlineEye fontSize={24} fill="#AFB2BF" />
+                )}
+              </span>
+              {errors.oldPassword && (
+                <span className="-mt-1 text-[12px] text-yellow-100">
+                  Please enter your Current Password.
+                </span>
+              )}
+            </div>
+            <div className="relative flex flex-col gap-2 lg:w-[48%]">
+              <label htmlFor="newPassword" className="text-pure-greys-200">
+                New Password
+              </label>
+              <input
+                type={showNewPassword ? "text" : "password"}
+                name="newPassword"
+                id="newPassword"
+                placeholder="Enter New Password"
+                className="outline-none px-2 py-4 rounded-md text-pure-greys-25 bg-richblue-700 placeholder:text-pure-greys-400 w-[100%]"
+                {...register("newPassword", { required: true })}
+              />
+              <span
+                onClick={() => setShowNewPassword((prev) => !prev)}
+                className="absolute right-3 top-[50px] z-[10] cursor-pointer"
+              >
+                {showNewPassword ? (
+                  <AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF" />
+                ) : (
+                  <AiOutlineEye fontSize={24} fill="#AFB2BF" />
+                )}
+              </span>
+              {errors.newPassword && (
+                <span className="-mt-1 text-[12px] text-yellow-100">
+                  Please enter your New Password.
+                </span>
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-end gap-2">
+          <button
+            className="cursor-pointer rounded-md bg-richblack-700 py-2 px-5 font-semibold text-richblack-50"
+            onClick={() => {
+              navigate("/dashboard/my-profile");
+            }}
+          >
+            Cancel
+          </button>
+
+          <button className="p-2 rounded-md outline-none font-semibold bg-yellow-100 flex gap-2 items-center text-center">
+            Update
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default UpdatePassword;
--- client/src/pages/Dashboard/Settings.jsx
+++ client/src/pages/Dashboard/Settings.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import DashboardWrapper from '../../components/Wrapper/DashboardWrapper'
 import DeleteAccount from '../../components/Dashboard/Settings/DeleteAccount'
+import UpdatePassword from '../../components/Dashboard/Settings/UpdatePassword'
 
 const Settings = () => {
   return (
     <>
         <DashboardWrapper>
             <div className='w-full md:w-[80%] lg:w-[1200px] mx-auto'>
+              <UpdatePassword />
               <DeleteAccount />
             </div>
         </DashboardWrapper>
