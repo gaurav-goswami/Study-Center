f6cff7d7e0454e32420d2c067ecc8339f5492007
Gaurav Goswami <gauravgoswami.aug@gmail.com>
2023-07-28 23:26:40 +0530

    added some courses related api

--- client/src/services/Courses.jsx
+++ client/src/services/Courses.jsx
@@ -9,18 +9,15 @@ const coursesApi = createApi({
 
         // get enrolled courses (student)
         getEnrolledCourses : builder.query({
-            query : ({token}) => ({
+            query : () => ({
                 url : "/profile/get-enrolled-courses",
                 method : "GET",
-                headers : {
-                    "Authorization" : `Bearer ${token}`
-                },
                 credentials : "include"
             })
         }),
 
         // create course (instructor only)
-        createCourse : builder.query({
+        createCourse : builder.mutation({
             query : (courseDetails) => ({
                 url : "/course/create-course",
                 method : "POST",
@@ -44,9 +41,61 @@ const coursesApi = createApi({
         getCourseDetails : builder.query({
             query : ({courseID}) => ({
                 url : `/course/get-course-details/${courseID}`,
-                method : "GET"
+                method : "GET",
+                params : {
+                    courseID
+                }
             })
         }),
+
+        // (student only)
+        updateCourseProgress : builder.mutation({
+            query : (details) => ({
+                url : "/course/update-course-progress",
+                method : "POST",
+                headers : {
+                    "Content-Type" : "application/json"
+                },
+                body : JSON.stringify(details),
+                credentials : "include"
+            })
+        }),
+
+        // course full detail
+        fullCourseDetails : builder.query({
+            query : ({courseId}) => ({
+                url : `/course/full-course-details/${courseId}`,
+                method : "GET",
+                credentials : "include",
+                params : {
+                    courseId
+                }
+            })
+        }),
+
+        // edit course (instructor only)
+        editCourse : builder.mutation({
+            query : (updates) => ({
+                url : "/course/editCourse",
+                method : "PUT",
+                headers : {
+                    "Content-Type" : "application/json"
+                },
+                body : JSON.stringify(updates),
+                credentials : "include"
+            })
+        }),
+
+        // instructor courses (instructor only)
+        instructorCourse : builder.query({
+            query : () => ({
+                url : "/course/instructor-course",
+                method : "GET",
+                credentials : "include"
+            })
+        }),
+
+        
 
     })  
 })
